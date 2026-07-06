871ac465f65a63ae781590e57e8e855da5673537
Daniele-rolli <danielerolli@proton.me>
2026-07-06 12:57:11 +0200

    Remove unused command param, add workflow path to deploy trigger

--- store/vite.config.js
+++ store/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [vue()],
   base: '/',
   build: {
     outDir: 'dist',
   },
-}));
+});
